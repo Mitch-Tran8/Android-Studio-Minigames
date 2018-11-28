4db06ab46da731e67dc810943cabc21b7f0d840a
Mitchell <mitchell.tran@mail.utoronto.ca>
2018-11-28 04:40:50 -0500

    Added unittests for the connect 4 two player game mode testing if the player1Wins(), player2Wins() and tie() methods properly changed their values.

--- Phase2/GameCenter/app/src/test/java/phase1/gamecenter/ConnectFourMainActivityTest.java
+++ Phase2/GameCenter/app/src/test/java/phase1/gamecenter/ConnectFourMainActivityTest.java
@@ -149,4 +149,74 @@ public class ConnectFourMainActivityTest {
         assertEquals(false, testerActivity.gameOver());
     }
 
+    @Test
+    public void testPlayer1WinsPlayer1PointsChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player1Wins();
+        assertEquals(5, testerActivity.getPlayer1Points());
+    }
+
+    @Test
+    public void testPlayer1WinsPlayer2PointsChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player1Wins();
+        assertEquals(-3, testerActivity.getPlayer2Points());
+    }
+
+    @Test
+    public void testPlayer1WinsPlayer1RoundChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player1Wins();
+        assertEquals(1, testerActivity.getPlayer1RoundsWon());
+    }
+
+    @Test
+    public void testPlayer1WinsRoundsPlayedChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player1Wins();
+        assertEquals(1, testerActivity.getRoundsPlayed());
+    }
+
+    @Test
+    public void testPLayer2WinsPlayer1PointsChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player2Wins();
+        assertEquals(-3, testerActivity.getPlayer1Points());
+    }
+
+    @Test
+    public void testPlayer2WinsPlayer2PointsChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player2Wins();
+        assertEquals(5, testerActivity.getPlayer2Points());
+    }
+
+    @Test
+    public void testPlayer2WinsPlayer2RoundChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player2Wins();
+        assertEquals(1, testerActivity.getPlayer2RoundsWon());
+    }
+
+    @Test
+    public void testPlayer2WinsRoundsPlayedChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.player2Wins();
+        assertEquals(1, testerActivity.getRoundsPlayed());
+    }
+
+    @Test
+    public void testTieRoundChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.tie();
+        assertEquals(1, testerActivity.getTies());
+    }
+
+    @Test
+    public void testTieRoundsPlayedChange(){
+        ConnectFourMainActivity testerActivity = new ConnectFourMainActivity();
+        testerActivity.tie();
+        assertEquals(1, testerActivity.getRoundsPlayed());
+    }
+
 }
