806f85ab81a9095758596eb829693d01a266d1f5
Mitchell <mitchell.tran@mail.utoronto.ca>
2018-11-29 18:32:52 -0500

    Refactored code in all of the connect 3 and 4 game modes, moving some methods to superclass ConnectNumbersActivity

--- Phase2/GameCenter/app/src/main/java/phase1/gamecenter/connectnumbers/ConnectThreeAIHardMainActivity.java
+++ Phase2/GameCenter/app/src/main/java/phase1/gamecenter/connectnumbers/ConnectThreeAIHardMainActivity.java
@@ -12,7 +12,7 @@ import java.util.Stack;
 
 import phase1.gamecenter.R;
 
-public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements View.OnClickListener {
+public class ConnectThreeAIHardMainActivity extends ConnectNumbersActivity implements View.OnClickListener {
 
     /**
      * 2D array of buttons, representing the connect three game board.
@@ -20,70 +20,11 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
     private Button[][] buttons = new Button[3][3];
 
     /**
-     * The number of moves made on the connect three board.
-     */
-    private int moves;
-
-    /**
-     * The number of games tied.
-     */
-    private int ties;
-
-    /**
-     * The score of player 1.
-     */
-    private int player1points;
-
-    /**
-     * The score of the AI.
-     */
-    private int aipoints;
-
-    /**
-     * TextView that shows the number of rounds player 1 has won.
-     */
-    private TextView scorePlayer1;
-
-    /**
      * TextView that shows the number of rounds the AI has won.
      */
     private TextView aiPlayer;
 
-    /**
-     * TextView that shows the number of ties.
-     */
-    private TextView draws;
-
-    /**
-     * Number of rounds won by player 1.
-     */
-    private int player1RoundsWon;
-
-    /**
-     * Number of rounds won by the AI.
-     */
-    private int aiRoundsWon;
-
-    /**
-     * Number of rounds played.
-     */
-    private int roundsPlayed;
-
-    /**
-     * Boolean representing if it is player 1's turn.
-     */
-    private boolean player1Turn = true;
-
-    /**
-     * The number of turns that have past.
-     */
     private int turns;
-
-    /**
-     * The stack that stocks all previous moves
-     */
-    private Stack<Integer> moveStack;
-
 
 
     @Override
@@ -121,7 +62,7 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
      * On click listener for the game reset button -> reset the game.
      * @param gameReset game reset button.
      */
-    private void gameResetListener(Button gameReset) {
+    protected void gameResetListener(Button gameReset) {
         gameReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -136,9 +77,9 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
                 roundsPlayed = 0;
                 player1points = 0;
                 player1RoundsWon = 0;
-                aiRoundsWon = 0;
+                opponentRoundsWon = 0;
                 ties = 0;
-                updatePoints();
+                updateRoundsWon();
             }
         });
     }
@@ -147,7 +88,7 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
      * On click listener for the reset match button -> make a new match.
      * @param buttonReset
      */
-    private void buttonResetListener(Button buttonReset) {
+    protected void buttonResetListener(Button buttonReset) {
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -164,7 +105,7 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
                 } else {
                     if (player1RoundsWon == 3) {
                         Toast.makeText(getApplicationContext(), "Game Over. Player 1 wins! Please start a new game.", Toast.LENGTH_LONG).show();
-                    } else if (aiRoundsWon == 3){
+                    } else if (opponentRoundsWon == 3){
                         Toast.makeText(getApplicationContext(), "Game Over. AI wins! Please start a new game.", Toast.LENGTH_LONG).show();
                     } else {
                         Toast.makeText(getApplicationContext(), "Game Over. TIE! Please start a new game.", Toast.LENGTH_LONG).show();
@@ -179,7 +120,7 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
         if (gameOver()) {
             gameOverMessage();
         } else {
-            if (matchOver()) {
+            if (matchOver(3, buttons)) {
                 Toast.makeText(this, "Match over. Please start a new match.", Toast.LENGTH_LONG).show();
             } else {
                 if (!((Button) v).getText().toString().equals("")) {//checks if button contains empty string, if X or O then already used
@@ -194,7 +135,7 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
     /**
      * Activate the undo button.
      */
-    private void addUndoButtonListener(Button undoButton) {
+    protected void addUndoButtonListener(Button undoButton) {
         undoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -215,11 +156,11 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
             ((Button) v).setText("X"); // human move
             this.moveStack.push(v.getId());
             --moves;
-            if (matchOver()){
+            if (matchOver(3, buttons)){
                 if (player1Turn){
                     player1Wins();
                     Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_LONG).show();
-                    updatePoints();
+                    updateRoundsWon();
                 }
             }else {
                 player1Turn = false;
@@ -228,11 +169,11 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
                     button.setText("O");
                     this.moveStack.push(button.getId());
                     --moves;
-                    if (matchOver()){
+                    if (matchOver(3, buttons)){
                         if (!player1Turn){
-                            aiWins();
+                            opponentWins();
                             Toast.makeText(this, "AI wins!", Toast.LENGTH_LONG).show();
-                            updatePoints();
+                            updateRoundsWon();
                         }
                     }
                 }
@@ -242,202 +183,39 @@ public class ConnectThreeAIHardMainActivity extends AppCompatActivity implements
         if (turns == 5){
                 tie();
                 Toast.makeText(this, "Tied!", Toast.LENGTH_LONG).show();
-                updatePoints();
+            updateRoundsWon();
             }else {
             player1Turn = true;
         }
     }
 
     /**
-     * set the number of rounds player 1 has won only for testing purpose
-     * @param round amount of rounds to be set
-     */
-    public void setPlayer1RoundsWon(int round){ this.player1RoundsWon = round;}
-
-    /**
-     * get the number of rounds player 1 has won only for testing purpose
-     */
-    public int getPlayer1RoundsWon(){ return player1RoundsWon;}
-
-    /**
-     * set the number of rounds the AI has won only for testing purpose
-     * @param round amount of rounds to be set
-     */
-    public void setAiRoundsWon(int round){ this.aiRoundsWon = round;}
-
-    /**
-     * get the number of rounds the AI has won only for testing purpose
-     */
-    public int getAiRoundsWon(){ return aiRoundsWon;}
-
-    /**
-     * set the number of rounds that have been played for testing purpose
-     * @param round amount of rounds to be set
-     */
-    public void setRoundsPlayed(int round){ this.roundsPlayed = round;}
-
-    /**
-     * get the number of rounds that have been played for testing purpose
-     */
-    public int getRoundsPlayed(){ return roundsPlayed;}
-
-    /**
-     * get the number of rounds player 1 has won only for testing purpose
-     */
-    public int getPlayer1Points(){ return player1points;}
-
-    /**
-     * get the number of rounds the AI has won only for testing purpose
-     */
-    public int getAiPoints(){ return aipoints;}
-
-    /**
-     * get the number of rounds that have been played for testing purpose
-     */
-    public int getTies(){ return ties;}
-
-
-    /**
      * Displays the toast message when the game is over.
      */
-    private void gameOverMessage() {
+    protected void gameOverMessage() {
         if (player1RoundsWon == 3) {
             Toast.makeText(this, "Game Over. Player 1 wins! Please start a new game.", Toast.LENGTH_LONG).show();
-        } else if (aiRoundsWon == 3) {
-            Toast.makeText(this, "Game Over. Player 2 wins! Please start a new game.", Toast.LENGTH_LONG).show();
+        } else if (opponentRoundsWon == 3) {
+            Toast.makeText(this, "Game Over. AI wins! Please start a new game.", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "Game Over. TIE! Please start a new game.", Toast.LENGTH_LONG).show();
         }
     }
 
     /**
-     * Player 1 wins the match, update scores.
+     * Update TextView with the scores of each player.
      */
-    public void player1Wins() {
-        player1points = player1points + 5;
-        aipoints = aipoints - 3;
-        player1RoundsWon++;
-        roundsPlayed++;
-    }
-
-    /**
-     * The AI wins the match, update scores.
-     */
-    public void aiWins() {
-        aipoints = aipoints + 5;
-        player1points = player1points - 3;
-        aiRoundsWon++;
-        roundsPlayed++;
-    }
-
-    /**
-     * Tie in the game.
-     */
-    public void tie() {
-        ties++;
-        roundsPlayed++;
-    }
-
-    /**
-     * Return whether the connect three game is over, that is, if the player or the AI has won three
-     * rounds or five rounds have been played without the player or AI winning 3 rounds.
-     *
-     * @return whether the game is over.
-     */
-    public boolean gameOver() {
-        return (player1RoundsWon == 3 || aiRoundsWon == 3 || roundsPlayed == 5);
-    }
-
-    /**
-     * Return whether the connect three game is over, that is, if the player or AI has made three in
-     * a row.
-     *
-     * @return whether the game is over.
-     */
-    private boolean matchOver() {
-        String[][] board = new String[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = buttons[i][j].getText().toString(); //go thru all buttons and set their XO text
-            }
-        }
-
-        return (checkRows(board) || checkColumns(board) || checkAscendingDiagonals(board) || checkDescendingDiagonals(board));
-    }
-
-    /**
-     * Return whether or not there is a 3 in a row.
-     *
-     * @param board String[][] with the current moves on the board (X's and O's)
-     * @return whether or not there is a 3 in a row.
-     */
-    public boolean checkRows(String[][] board) {
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2]) &&
-                    !board[i][0].equals("")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Return whether or not there is a 3 in a column.
-     *
-     * @param board String[][] with the current moves on the board (X's and O's)
-     * @return whether or not there is a 3 in a column
-     */
-    public boolean checkColumns(String[][] board) {
-        for (int i = 0; i < 3; i++) {
-            if (board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i]) &&
-                    !board[0][i].equals("")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Return whether or not there is a 3 in an ascending diagonal pattern.
-     *
-     * @param board String[][] with the current moves on the board (X's and O's)
-     * @return whether or not there is a 3 in a ascending diagonal.
-     */
-    public boolean checkAscendingDiagonals(String[][] board) {
-
-        return (board[0][2].equals(board[1][1]) &&
-                board[0][2].equals(board[2][0]) &&
-                !board[0][2].equals(""));
-    }
-
-    /**
-     * Return whether or not there is a 3 in an descending diagonal pattern.
-     *
-     * @param board String[][] with the current moves on the board (X's and O's)
-     * @return whether or not there is a 3 in a descending diagonal.
-     */
-    public boolean checkDescendingDiagonals(String[][] board) {
-
-        return (board[0][0].equals(board[1][1]) &&
-                board[0][0].equals(board[2][2]) &&
-                !board[0][0].equals(""));
-    }
-
-    /**
-     * Update TextView with the scores of the player and AI.
-     */
-    private void updatePoints() {
+    protected void updateRoundsWon() {
         scorePlayer1.setText("Player 1: " + player1RoundsWon);
-        aiPlayer.setText("AI: " + aiRoundsWon);
+        aiPlayer.setText("AI: " + opponentRoundsWon);
         draws.setText("Draws: " + ties);
     }
 
     /**
      * undo the most recent move if the max undo times has not been reached
      */
-    private void undoMove() {
-        if(turns < 5){
+    protected void undoMove() {
+        if(!matchOver(3, buttons) || turns < 5){
             if(moveStack.size() > 0){
                 int id = this.moveStack.pop();
                 for (int i = 0; i < 3; i++) {
